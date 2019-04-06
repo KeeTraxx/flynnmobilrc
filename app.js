cb82c6287f68a7642959cae28066a68bf95a9745
Marc Schmid <m4rc@gmx.ch>
2019-04-06 15:08:22 +0200

    Added config.js file in order to configure mqtt mobile rc. Fixed broken interval check. Added logic in order to periodically send message even if no drag was done. Limit MQTT messages to 5/s.

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
-let client  = mqtt.connect('ws://broker.mqttdashboard.com:8000/mqtt')
-client.publish('ch/compile/flynnmobil/test', 'hello')
+let client  = mqtt.connect(BROKER)
 let svg = d3.select('#svg')
 
 let width;
@@ -19,8 +18,8 @@ d3.select(window).on("resize", updateSizes)
 updateSizes()
 
 let drag = d3.drag()
-  .on('start', () => onDrag())
-  .on('drag', () => onDrag())
+  .on('start', () => onDrag(true))
+  .on('drag', () => onDrag(false))
   .on('end', () => onEnd())
 
 svg.call(drag)
@@ -32,50 +31,78 @@ let rectScaler = d3.scaleLinear().domain([0, 1]).range([0, 200])
 
 let lastmillis = new Date().getTime()
 
-function onDrag () {
-  let accel = accelerationScale(d3.event.y)
-  let turn = turningScale(d3.event.x)
+let leftservo = 0
+let rightservo = 0
 
-  // https://electronics.stackexchange.com/questions/19669/algorithm-for-mixing-2-axis-analog-input-to-control-a-differential-motor-drive
+let intervalHandle = null
 
-  let k = 1
+let count = 0
 
-  let leftservo = accel + turn
-  let rightservo = accel - turn
+onEnd()
 
-  if (Math.abs(leftservo) > 1) {
-    k = 1 / Math.abs(leftservo)
+function onDrag (start) {
+  if(start) {
+    console.log("START")
+    intervalHandle = setInterval(onDrag, 100)
   }
 
-  if (Math.abs(rightservo) > 1) {
-    k = 1 / Math.abs(rightservo)
+  if (d3.event) {
+    let accel = accelerationScale(d3.event.y)
+    let turn = turningScale(d3.event.x)
+  
+    // https://electronics.stackexchange.com/questions/19669/algorithm-for-mixing-2-axis-analog-input-to-control-a-differential-motor-drive
+
+    let k = 1
+
+    leftservo = accel + turn
+    rightservo = accel - turn
+
+    if (Math.abs(leftservo) > 1) {
+          k = 1 / Math.abs(leftservo)
+    }
+
+    if (Math.abs(rightservo) > 1) {
+          k = 1 / Math.abs(rightservo)
+    }
+
+    leftservo *= k
+    rightservo *= k
+
+    leftservoRect.attr('transform', leftservo > 0 ? 'rotate(180)' : '')
+    leftservoRect.attr('height', rectScaler(Math.abs(leftservo)))
+
+    rightservoRect.attr('transform', rightservo > 0 ? 'rotate(180)' : '')
+    rightservoRect.attr('height', rectScaler(Math.abs(rightservo)))
   }
-
-  leftservo *= k
-  rightservo *= k
-
-  console.log(leftservo, rightservo)
-
-  leftservoRect.attr('transform', leftservo > 0 ? 'rotate(180)' : '')
-  leftservoRect.attr('height', rectScaler(Math.abs(leftservo)))
-
-  rightservoRect.attr('transform', rightservo > 0 ? 'rotate(180)' : '')
-  rightservoRect.attr('height', rectScaler(Math.abs(rightservo)))
 
   let currentTime = new Date().getTime()
 
-  if (currentTime - lastmillis > 100) {
-    client.publish('ch/compile/flynnmobil/leftservo', '' + Math.round(leftservo * 127));
-    client.publish('ch/compile/flynnmobil/rightservo', '' + Math.round(-rightservo * 127));
-  }
+  if ((currentTime - lastmillis) > 200) {
+    let left =  Math.round(leftservo * LEFT_SERVO_RANGE) + LEFT_SERVO_STOP
+    let right =  Math.round(rightservo * RIGHT_SERVO_RANGE) + RIGHT_SERVO_STOP
 
-  lastmillis = currentTime
+    count++
+    console.log(`[${count}] leftservo:${leftservo.toFixed(2)} rightservo:${rightservo.toFixed(2)}`)
+    console.log(`[${count}] left:${left} right:${right}`)
+    
+    client.publish(LEFT_TOPIC, `${left}`)
+    client.publish(RIGHT_TOPIC, `${right}`)
+    
+    lastmillis = currentTime
+  }
 }
 
 function onEnd() {
-  client.publish('ch/compile/flynnmobil/leftservo', '0')
-  client.publish('ch/compile/flynnmobil/rightservo', '0')
+  if (intervalHandle){
+    clearInterval(intervalHandle)
+    intervalHandle = 0
+  }
+  leftservo = 0
+  rightservo = 0
+  client.publish(LEFT_TOPIC, `${LEFT_SERVO_STOP}`)
+  client.publish(RIGHT_TOPIC, `${RIGHT_SERVO_STOP}`)
 
   leftservoRect.attr('height', rectScaler(0))
   rightservoRect.attr('height', rectScaler(0))
+  console.log("STOP!")
 }
